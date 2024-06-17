b847dec9683610193c9dc5eda338ef8fd0004a6a
Maxsuell F <89057455+MxaYa@users.noreply.github.com>
2024-06-17 20:41:08 -0300

    novo commit

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
+require("dotenv").config(); // Carrega as variáveis de ambiente do arquivo .env
+require("dotenv").config({ path: "./.env" });
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASS:", process.env.DB_PASS);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
 
 
 const connection = require("./database/database");
