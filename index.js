9dc25306315584e2f06816f8115ba4d9dbff0dd3
Maxsuell F <89057455+MxaYa@users.noreply.github.com>
2024-06-18 20:52:16 -0300

    Portas

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_PASS:", process.env.DB_PASS);
 console.log("DB_NAME:", process.env.DB_NAME);
 console.log("DB_PORT:", process.env.DB_PORT);
+console.log("PORT:", process.env.PORT);
 
-
+const port = process.env.PORT || 1046;
 
 
 const connection = require("./database/database");
