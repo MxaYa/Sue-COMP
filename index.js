8a1d3e2fa3a9eaf26a9f4746204b5d5d36a6d092
Maxsuell F <89057455+MxaYa@users.noreply.github.com>
2024-06-18 18:43:51 -0300

    agora funciona

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require("dotenv").config(); // Carrega as variáveis de ambiente do arquivo .env
+// Carrega as variáveis de ambiente do arquivo .env
 require("dotenv").config({ path: "./.env" });
+
 console.log("DB_HOST:", process.env.DB_HOST);
 console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_PASS:", process.env.DB_PASS);
@@ -18,7 +19,7 @@ console.log("DB_NAME:", process.env.DB_NAME);
 console.log("DB_PORT:", process.env.DB_PORT);
 console.log("PORT:", process.env.PORT);
 
-const port = process.env.PORT || 1046;
+const port = process.env.PORT || 4000;
 
 const connection = require("./database/database");
 
